1e43bc1dca240892ff62caced3d8ef755e6527cf
TheeraphatCH <theeraphatchueanokkhum@gmail.com>
2025-02-18 20:52:33 +0700

    feat: shortcut icon

---
+++ final_project/code/view/event/AttendeeView.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eat With Me</title>
+</head>
+
+<body>
+    <div
+        class="flex flex-col justify-center items-center gap-12 pt-[200px] pr-10 pb-[200px] pl-10 w-[1940px] h-[1713px] bg-teal-700">
+        <div
+            class="flex flex-col justify-start items-center gap-12 w-[1200px] shadow-sm">
+            <div
+                class="flex justify-between items-end pb-7 pr-8 pl-[52px] gap-[740px] w-[1650px] h-[690px] rounded-[50px] bg-ImageAsset2 overflow-hidden">
+                <div class="flex flex-col justify-start items-start h-[623px]">
+                    <div
+                        class="flex flex-row justify-center items-center gap-2 pt-2 pr-4 pb-2 pl-4 rounded-lg h-11 shadow-sm bg-orange-50 min-w-[119px]">
+                        <img
+                            width="8px"
+                            height="16px"
+                            src="public/icons/drop.svg"
+                            alt="Back"
+                            class="transform -rotate-90" />
+                        <div
+                            class="font-kanit text-lg min-w-[72px] whitespace-nowrap text-teal-700 text-opacity-100 text-center leading-none font-light">
+                            ย้อนกลับ
+                        </div>
+                    </div>
+                    <div
+                        class="flex flex-col justify-start items-start gap-2.5 mt-[91px] h-[400px]">
+                        <div
+                            class="font-kanit text-3xl min-w-[440px] whitespace-nowrap text-orange-50 text-opacity-100 leading-none font-medium">
+                            Eat with me!
+                        </div>
+                        <div
+                            class="font-kanit text-base w-[440px] text-orange-50 text-opacity-100 leading-none font-normal">
+                            &quot;Eat with Me: How to Eat for Health&quot;<br />มาร่วมงาน
+                            &quot;Eat with Me&quot; กับเรา! 🌿✨<br />งานที่จะพาคุณเรียนรู้เกี่ยวกับการรับประทานอาหารอย่างถูกต้อง
+                            เพื่อสุขภาพที่ดีและสมดุล พบกับแนวทางการเลือกอาหารที่มีประโยชน์
+                            เคล็ดลับการกินเพื่อสุขภาพ และไอเดียเมนูอร่อยที่ดีต่อร่างกาย<br /><br />📅
+                            วันและเวลา: 12 มกราคาม 2568📍 สถานที่: Coworking space ท่าขอนยาง<br /><br />ร่วมสัมผัสประสบการณ์การกินอย่างมีสติ
+                            และค้นพบวิธีดูแลสุขภาพผ่านอาหารที่อร่อยและมีคุณค่าทางโภชนาการ! 🥗🍎
+                        </div>
+                    </div>
+                    <div
+                        class="mt-14 flex justify-between items-center gap-2 w-[149px] h-7">
+                        <img
+                            width="19.3px"
+                            height="20px"
+                            src="/assets/SvgAsset2.svg"
+                            alt="Svg Asset 2" />
+                        <div
+                            class="font-kanit text-[18px] min-w-[120px] whitespace-nowrap text-white text-opacity-100 leading-none font-normal">
+                            ดูแผนที่
+                        </div>
+                    </div>
+                </div>
+                <div
+                    class="flex flex-col justify-start items-start gap-8 pt-7 pr-7 pb-7 pl-7 rounded-2xl w-[385px] h-[284px] shadow-md bg-white">
+                    <div
+                        class="flex flex-col justify-start items-start gap-2.5 w-[325px] h-[73px]">
+                        <div
+                            class="font-kanit text-xl min-w-[325px] whitespace-nowrap text-neutral-800 text-opacity-100 leading-none font-normal">
+                            เวลาจัดงาน
+                        </div>
+                        <div
+                            class="font-kanit text-[18px] min-w-[325px] whitespace-nowrap text-neutral-400 text-opacity-100 leading-none font-normal">
+                            อาทิตย์ที่ 12 ม.ค. 2025 : 9.00 PM
+                        </div>
+                    </div>
+                    <div
+                        class="flex flex-col justify-center items-center gap-2.5 h-[118px]">
+                        <div
+                            class="flex justify-center items-center rounded w-[325px] h-14 bg-teal-700">
+                            <div
+                                class="font-kanit text-base min-w-[47px] whitespace-nowrap text-orange-50 text-opacity-100 text-center leading-none font-normal">
+                                เข้าร่วม
+                            </div>
+                        </div>
+                        <div
+                            class="flex justify-center items-center rounded border-teal-700 border-t-2 border-b-2 border-l-2 border-r-2 border-solid border-2 w-[325px] h-14">
+                            <div
+                                class="font-kanit text-base min-w-[34px] whitespace-nowrap text-teal-700 text-opacity-100 text-center leading-none font-normal">
+                                สนใจ
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col justify-start items-start gap-12 h-[580px]">
+            <div
+                class="flex flex-row justify-between items-start gap-12 w-[1200px] h-[310px]">
+                <div class="flex flex-col justify-start items-start gap-2.5 h-[310px]">
+                    <div
+                        class="font-kanit text-xl min-w-[550px] whitespace-nowrap text-orange-50 text-opacity-100 leading-none font-semibold">
+                        คำอธิบาย
+                    </div>
+                    <div
+                        class="font-kanit text-base w-[550px] text-orange-50 text-opacity-100 leading-none font-normal">
+                        &quot;Eat with Me: How to Eat for Health&quot;<br />มาร่วมงาน
+                        &quot;Eat with Me&quot; กับเรา! 🌿✨<br />งานที่จะพาคุณเรียนรู้เกี่ยวกับการรับประทานอาหารอย่างถูกต้อง
+                        เพื่อสุขภาพที่ดีและสมดุล พบกับแนวทางการเลือกอาหารที่มีประโยชน์
+                        เคล็ดลับการกินเพื่อสุขภาพ และไอเดียเมนูอร่อยที่ดีต่อร่างกาย<br /><br />📅
+                        วันและเวลา: 12 มกราคาม 2568📍 สถานที่: Coworking space ท่าขอนยาง<br /><br />ร่วมสัมผัสประสบการณ์การกินอย่างมีสติ
+                        และค้นพบวิธีดูแลสุขภาพผ่านอาหารที่อร่อยและมีคุณค่าทางโภชนาการ! 🥗🍎
+                    </div>
+                </div>
+                <div class="flex flex-col justify-start items-start gap-2.5 h-[310px]">
+                    <div
+                        class="font-kanit text-xl min-w-[550px] whitespace-nowrap text-orange-50 text-opacity-100 leading-none font-normal">
+                        สถานที่จัดงาน
+                    </div>
+                    <div
+                        class="flex justify-end items-end pb-4 pr-5 w-[550px] h-[260px] bg-ImageAsset1">
+                        <div
+                            class="flex flex-row justify-start items-center gap-2.5 pt-1 pr-2.5 pb-1 pl-2.5 rounded h-8 bg-neutral-400/50 min-w-[99px]">
+                            <div
+                                class="font-kanit text-base min-w-[49px] whitespace-nowrap underline text-neutral-800 text-opacity-100 leading-none font-normal">
+                                <span class="underline">คัดลอก</span>
+                            </div>
+                            <div
+                                class="flex flex-row justify-center items-center gap-2.5 rounded-[80px] h-5 bg-orange-50 overflow-hidden">
+                                <img
+                                    width="13.5px"
+                                    height="13.5px"
+                                    src="/assets/SvgAsset1.svg"
+                                    alt="Svg Asset 1" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div
+                class="flex flex-row justify-between items-center gap-12 w-[1200px] h-[83px]">
+                <div class="flex flex-col justify-start items-start gap-5 h-[83px]">
+                    <div
+                        class="font-kanit text-xl min-w-[550px] whitespace-nowrap text-orange-50 text-opacity-100 leading-none font-semibold">
+                        เวลา
+                    </div>
+                    <div
+                        class="flex flex-row justify-start items-start gap-5 w-[550px] h-7">
+                        <div
+                            class="font-kanit text-base min-w-[64px] whitespace-nowrap text-orange-50 text-opacity-100 leading-none font-normal">
+                            วันอาทิตย์
+                        </div>
+                        <div
+                            class="font-kanit text-[18px] min-w-[66px] whitespace-nowrap text-amber-400 text-opacity-100 leading-none font-normal">
+                            9.00 PM
+                        </div>
+                    </div>
+                </div>
+                <div
+                    class="flex flex-col justify-start items-start gap-2 w-[550px] h-[83px]">
+                    <div
+                        class="font-kanit text-xl min-w-[550px] whitespace-nowrap text-orange-50 text-opacity-100 leading-none font-semibold">
+                        มหาวิทยาลัยมหาสารคาม
+                    </div>
+                    <div
+                        class="font-kanit text-base min-w-[480px] whitespace-nowrap text-orange-50 text-opacity-100 leading-none font-normal">
+                        41 ตำบล ขามเรียง อำเภอกันทรวิชัย มหาสารคาม 44150
+                    </div>
+                </div>
+            </div>
+            <div
+                class="flex flex-col justify-start items-end gap-2.5 w-[1200px] h-[87px]">
+                <div class="flex flex-col justify-start items-start gap-5 h-20">
+                    <div
+                        class="font-kanit text-xl min-w-[550px] whitespace-nowrap text-orange-50 text-opacity-100 leading-none font-semibold">
+                        หัวข้อ
+                    </div>
+                    <div class="flex flex-row justify-start items-start gap-2.5 w-[550px]">
+                        <div
+                            class="flex justify-center items-center rounded w-[87px] h-8 bg-orange-50">
+                            <div
+                                class="font-kanit text-sm min-w-[67px] whitespace-nowrap text-teal-700 text-opacity-100 text-center leading-none font-normal">
+                                EatWithMe
+                            </div>
+                        </div>
+                        <div
+                            class="flex justify-center items-center rounded w-[119px] h-8 bg-orange-50">
+                            <div
+                                class="font-kanit text-sm min-w-[99px] whitespace-nowrap text-teal-700 text-opacity-100 text-center leading-none font-normal">
+                                กินอย่างมีสุขภาพ
+                            </div>
+                        </div>
+                        <div
+                            class="flex justify-center items-center rounded w-[136px] h-8 bg-orange-50">
+                            <div
+                                class="font-kanit text-sm min-w-[116px] whitespace-nowrap text-teal-700 text-opacity-100 text-center leading-none font-normal">
+                                สุขภาพดีเริ่มที่อาหาร
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
