c3e35f72d8a103ae9e1442602852ef76a79c6ab8
TheeraphatCH <theeraphatchueanokkhum@gmail.com>
2025-02-19 17:01:56 +0700

    feat: setup map encode lat and lon

--- final_project/code/components/map/map.php
+++ final_project/code/components/map/map.php
@@ -34,22 +34,52 @@ class Map extends Component
                 Init
             } from '/code/components/map/map.js';
 
+            // async function fetchAddress(lat, lon) {
+            //     const response = await fetch(`/fetch-address.php?lat=${lat}&lon=${lon}`);
+            //     return await response.json();
+            // }
+
             document.addEventListener('DOMContentLoaded', () => {
                 const init = new Init();
 
                 const location = document.getElementById('location');
 
-                document.addEventListener('locationChanged', (event) => {
+                document.addEventListener('locationChanged', async (event) => {
                     const {
                         lat,
                         lon
                     } = event.detail;
                     console.log(`${lat} | ${lon}`);
+
+                    // const res = await fetchAddress($lat, $lon);
+                    // console.log(res);
+
                     location.textContent = `${lat}, ${lon}`;
                 });
             });
         </script>
 <?php
 
+    }
+
+    private function fetchAddress($lat, $lon)
+    {
+        $url = `https://api.longdo.com/map/services/address?lon=$lon&lat=$lat&noelevation=1&key={$this->mapApiKey}`;
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+
+        $response = curl_exec($ch);
+
+        if (curl_errno($ch)) {
+            error_log('Curl error: ' . curl_error($ch));
+            return false;
+        }
+
+        curl_close($ch);
+
+        return json_decode($response, true);
     }
 }
